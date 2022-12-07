f6f8b9ab693d2e0f7b30cb8e0e1627bc93d3682a
Juan Alfredo <38137911+alfredojuareztamayo@users.noreply.github.com>
2022-12-07 13:50:58 -0600

    mov aleatorio enemigo2

---
+++ VRtesting/Assets/PlayerTest/Player3D.cs
@@ -0,0 +1,98 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player3D : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float speed;
+    public Animator animator;
+    public Transform eje;
+    public bool inground;
+    private RaycastHit Hit;
+    public float distancia;
+    public Vector3 v3;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Physics.Raycast(transform.position + v3, transform.up*-1,out Hit, distancia))
+        {
+            if (Hit.collider.tag == "piso")
+            {
+                inground = true;
+
+            }
+            else
+            {
+                inground = false;
+            }
+        }
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawRay(transform.position + v3, Vector3.up * -1 * distancia);
+    }
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        Vector3 RotaTargetZ = eje.transform.forward;
+        RotaTargetZ.y = 0;
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(RotaTargetZ), 0.3f);
+            var dir = transform.forward * speed * Time.fixedDeltaTime;
+            dir.y = rb.velocity.y;
+            rb.velocity = dir;
+            animator.SetBool("run", true);
+
+        }
+        else
+        {
+            if (inground)
+            {
+                rb.velocity = Vector3.zero;
+                    }
+            animator.SetBool("run", false);
+
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(RotaTargetZ * -1), 0.3f);
+            var dir = transform.forward * speed * Time.fixedDeltaTime;
+            dir.y = rb.velocity.y;
+            rb.velocity = dir;
+            animator.SetBool("run", true);
+           
+        }
+        Vector3 RotaTargetX = eje.transform.right;
+        RotaTargetX.y = 0;
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(RotaTargetX), 0.3f);
+            var dir = transform.forward * speed * Time.fixedDeltaTime;
+            dir.y = rb.velocity.y;
+            rb.velocity = dir;
+            animator.SetBool("run", true);
+          
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(RotaTargetX * -1), 0.3f);
+            var dir = transform.forward * speed * Time.fixedDeltaTime;
+            dir.y = rb.velocity.y;
+            rb.velocity = dir;
+            animator.SetBool("run", true);
+
+        }
+
+    }
+}
--- VRtesting/Assets/Scripts/EnemyMedio/LogicMovmentEnemy2.cs
+++ VRtesting/Assets/Scripts/EnemyMedio/LogicMovmentEnemy2.cs
@@ -10,20 +10,24 @@ public class LogicMovmentEnemy2 : MonoBehaviour
     public Quaternion angulo;
     public float grado;
 
+    public GameObject target;
 
     void Start()
     {
         animator = GetComponent<Animator>();
+        target = GameObject.Find("Papa");
     }
 
     // Update is called once per frame
     void Update()
     {
-      
+        comportamientoEnemigo();
     }
     public void comportamientoEnemigo()
     {
-        cronometro += 1 * Time.deltaTime;
+        if (Vector3.Distance(transform.position, target.transform.position)>5) {
+            animator.SetBool("Run", false);
+            cronometro += 1 * Time.deltaTime;
         if (cronometro >= 4)
         {
             rutine = Random.Range(0, 2);
@@ -32,8 +36,29 @@ public class LogicMovmentEnemy2 : MonoBehaviour
         switch (rutine)
         {
             case 0:
-                animator.SetBool("WalkEnemy2", false);
+                animator.SetBool("walk", false);
                 break;
+            case 1:
+                grado = Random.Range(0, 360);
+                angulo = Quaternion.Euler(0, grado, 0);
+                rutine++;
+                break;
+            case 2:
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, angulo, 0.5f);
+                transform.Translate(Vector3.forward * 1 * Time.deltaTime);
+                animator.SetBool("walk", true);
+                break;
+
+        }
+        }else
+        {
+            var lookPos = target.transform.position - transform.position;
+            lookPos.y = 0;
+            var rotation = Quaternion.LookRotation(lookPos);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, 3);
+            animator.SetBool("walk", false);
+            animator.SetBool("Run", true);
+            transform.Translate(Vector3.forward * 2 * Time.deltaTime);
         }
     }
 }
